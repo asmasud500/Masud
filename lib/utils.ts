60268df56f3499b1f0aeb51b92d181f58ac77d15
asmasud500 <110842433+asmasud500@users.noreply.github.com>
2026-08-31 22:40:01 +0600

    Stage 5: add utility helpers

---
+++ lib/utils.ts
@@ -0,0 +1,1 @@
+export function cn(...classes:Array<string|false|null|undefined>){return classes.filter(Boolean).join(" ")}export function absoluteUrl(path=""){return new URL(path,process.env.NEXT_PUBLIC_SITE_URL||"http://localhost:3000").toString()}
